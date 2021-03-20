8f250b85b691409b10ad907ac9c1469df723cc0a
winter <yanc_see@outlook.com>
2021-03-20 13:09:55 +0800

    laravel init

---
+++ app/Http/Controllers/Wx/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace App\Http\Controllers\Wx;
+
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\Request;
+
+    class AuthController extends Controller
+    {
+        public function register(Request $request)
+        {
+            //
+            return 1;
+        }
+    }
